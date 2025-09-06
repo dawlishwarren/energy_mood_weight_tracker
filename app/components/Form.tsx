d3b401f1a194c6532a2a3c4cff1d3166c98aad66
dawlishwarren <dawlishwarren@gmail.com>
2025-09-06 11:50:04 +0100

    supabase fetch request set up

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,35 +1,48 @@
-"use client";
+'use client';
+
+import { createClient } from '@/utils/supabase/client';
 
 export default function Form() {
-	function formSubmit(e) {
+	const supabase = createClient();
+
+	async function formSubmit(e: any) {
 		e.preventDefault();
-		// Mood
 		const form = e.target;
 		const formData = new FormData(form);
-		const today = new Date().toISOString().split("T")[0];
-		formData.append("date", today);
-
+		const today = new Date().toISOString().split('T')[0];
+		formData.append('date', today);
 		const formJson = Object.fromEntries(formData.entries());
 		console.log(formJson);
+		const { error } = await supabase.from('data').insert({
+			energy: formJson.energy,
+			mood: formJson.mood,
+			weight: formJson.weight,
+			created_at: today,
+			notes: formJson.notes,
+		});
 	}
 	return (
-		<form onSubmit={formSubmit} className="flex flex-col">
+		<form onSubmit={formSubmit} className='flex flex-col'>
 			<div>
-				<input type="number" name="Energy" className="border mx-2" />
-				<label htmlFor="Energy">Energy</label>
+				<input type='number' name='energy' className='border mx-2' />
+				<label htmlFor='energy'>Energy</label>
 				<span> (1-10)</span>
 			</div>
 			<div>
-				<input type="number" name="Mood" className="border mx-2" />
-				<label htmlFor="Mood">Mood</label>
+				<input type='number' name='mood' className='border mx-2' />
+				<label htmlFor='mood'>Mood</label>
 				<span> (1-10)</span>
 			</div>
 			<div>
-				<input type="text" name="Weight" className="border mx-2" />
-				<label htmlFor="Weight">Weight</label>
+				<input type='text' name='weight' className='border mx-2' />
+				<label htmlFor='weight'>Weight</label>
 				<span> (kg)</span>
 			</div>
-			<button type="submit" className="border">
+			<div>
+				<input type='text' name='notes' className='border mx-2' />
+				<label htmlFor='notes'>Notes</label>
+			</div>
+			<button type='submit' className='border'>
 				Submit
 			</button>
 		</form>
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,26 @@
-import Image from "next/image";
-import Form from "./components/Form";
+import Image from 'next/image';
+import Form from './components/Form';
+import { createClient } from '@/utils/supabase/server';
 
-export default function Home() {
+export default async function Home() {
+	const supabase = await createClient();
+	const { data, error } = await supabase.from('data').select();
+	console.log(data, error);
+
 	return (
-		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+		<div className='font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
+			<main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
 				<Image
-					className="dark:invert"
-					src="/next.svg"
-					alt="Next.js logo"
+					className='dark:invert'
+					src='/next.svg'
+					alt='Next.js logo'
 					width={180}
 					height={38}
 					priority
 				/>
 				<Form />
 			</main>
-			<footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+			<footer className='row-start-3 flex gap-[24px] flex-wrap items-center justify-center'>
 				<p>Footer</p>
 			</footer>
 		</div>
